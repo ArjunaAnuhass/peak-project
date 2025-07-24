fbf5e09759c813fb6cee77989b5db18b5677d6a1
Arjuna Anuhass <anuhass2017@gmail.com>
2025-07-24 09:17:57 +0530

    announcement bar color and text changing effect

--- frontend/src/components/AnnouncementBar.jsx
+++ frontend/src/components/AnnouncementBar.jsx
@@ -1,11 +1,29 @@
 import { CircleQuestionMark, CircleUserRound } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Label } from "./ui/label";
 
 function AnnouncementBar() {
+
+  const [currentBar, setCurrentBar] = useState(false);
+
+  const bgColor = currentBar ? "bg-customGreen" : "bg-customYellow";
+  const text = currentBar ? "Free delivery to your door for orders over $100" : "Check out our new Protein Shake 20% off for a limited time!";
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentBar((prev) => !prev);
+    }, 5000);
+
+    return () => clearInterval(timer);
+  },[]);
+
+  
+
   return (
-    <div className="bg-customGreen h-10">
-      <div className="flex items-center justify-between px-4 max-w-7xl mx-auto h-full">
+    <div className={`${bgColor} h-10 transition-all duration-100`}>
+      <div className={"flex items-center justify-between px-4 max-w-7xl mx-auto h-full"}>
         <span className="text-white font-light text-xs sm:text-sm truncate">
-          Free delivery to your door for orders over $100
+          {text}
         </span>
         <div className="flex items-center space-x-4">
           <div className="flex items-center space-x-1 cursor-pointer">
